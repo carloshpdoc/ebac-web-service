811a1e7f6f3abeb87d17abc13b16204b9a3d95bf
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2021-12-13 18:28:17 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const homeData = require('./data.json')
+
+app.listen(8080, (error) => {
+    if (error) throw error
+    console.log('Server running in http://127.0.0.1:8080')
+})
+
+app.get('/', (req, res) => {
+    res.send('Hosting Successfully!')
+})
+
+app.get('/home', (req, res) => {
+    res.status(200).json({ home: homeData })
+})
