fe376fdc786f6536f7681b5f4e14616639d8b717
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2021-12-20 20:12:36 -0300

    included server with method post

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser")
+const morgan = require('morgan')
+const cors = require('cors')
 const homeData = require('./data.json')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors());
+app.use(morgan('dev'))
 
 app.listen(process.env.PORT || 8080, (error) => {
     if (error) throw error
@@ -13,4 +21,11 @@ app.get('/', (req, res) => {
 
 app.get('/home', (req, res) => {
     res.status(200).json({ home: homeData })
+})
+
+app.post('/login', (req, res) => {
+    var user_name = req.body.user;
+    var password = req.body.password;
+    console.log("User name = "+user_name+", password is "+password);
+    res.end("yes")
 })
